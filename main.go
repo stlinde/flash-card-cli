4a2e1383748c19647a421cc218a7de9724a194cd
Sebastian Tved Linde <sebastian@tved-linde.dk>
2022-04-14 13:12:12 +0200

    Changes to the main.go testing file

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/stlinde/flash-card-cli/pkg"
+import (
+    "github.com/stlinde/flash-card-cli/pkg"
+)
+
+// We need to set the location of the json file the first time the program is run.
 
 func main() {
     pkg.CreateFlashCard()
